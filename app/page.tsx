939a7cddca6d49c48de19c6ee3c7057022c6ba55
S-P-H-E <sphe.g.personal@gmail.com>
2025-08-21 18:07:50 +0200

    optimise

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
 
   return (
     <div className="h-[100dvh] flex flex-col items-center justify-center gap-6 p-6 motion-opacity-in-0 motion-preset-expand motion-blur-in-lg motion-duration-1500">
-      <Image src={img} alt={alt} width={500} height={500} className="aspect-video rounded-2xl motion-delay-150 motion-opacity-in-0 motion-preset-expand"/>
+      <Image src={img} alt={alt} width={500} height={500} className="aspect-video rounded-2xl motion-delay-150 motion-opacity-in-0 motion-preset-expand" priority unoptimized/>
       <h1 className="font-semibold text-5xl motion-delay-300 motion-opacity-in-0 motion-preset-expand">I build cool shi.</h1>
       <p className="motion-delay-500 motion-opacity-in-0 motion-preset-expand">check out my <Link href="https://qr.sssphe.com" className="underline">QR Code Generator</Link> tool</p>
     </div>
