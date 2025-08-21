95be6edaa198fcff9941e722fd91ace38dcf5441
S-P-H-E <sphe.g.personal@gmail.com>
2025-08-21 13:41:59 +0200

    main

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
+  const img = "https://media1.tenor.com/m/5i6frz9dX4UAAAAd/mr-beast-hood-mr-beast.gif"
+  const alt = "Salsa Dance Blue Shirt Buy"
+
   return (
-    <div>
-      
+    <div className="h-[100dvh] flex flex-col items-center justify-center gap-6">
+      <Image src={img} alt={alt} width={500} height={500} className="aspect-video rounded-2xl"/>
+      <h1 className="font-semibold text-5xl">I build cool shi.</h1>
+      <p>check out my <Link href="/" className="underline">QR Code Generator</Link> tool</p>
     </div>
-  );
+  )
 }
