5557e4d05aaaa537afb23c3915a15ed53587d7c1
S-P-H-E <sphe.g.personal@gmail.com>
2025-08-21 15:18:34 +0200

    animation

--- app/page.tsx
+++ app/page.tsx
@@ -6,10 +6,10 @@ export default function Home() {
   const alt = "Salsa Dance Blue Shirt Buy"
 
   return (
-    <div className="h-[100dvh] flex flex-col items-center justify-center gap-6">
-      <Image src={img} alt={alt} width={500} height={500} className="aspect-video rounded-2xl"/>
-      <h1 className="font-semibold text-5xl">I build cool shi.</h1>
-      <p>check out my <Link href="https://qr.sssphe.com" className="underline">QR Code Generator</Link> tool</p>
+    <div className="h-[100dvh] flex flex-col items-center justify-center gap-6 p-6 motion-opacity-in-0 motion-preset-expand motion-blur-in-lg motion-duration-1500">
+      <Image src={img} alt={alt} width={500} height={500} className="aspect-video rounded-2xl motion-delay-150 motion-opacity-in-0 motion-preset-expand"/>
+      <h1 className="font-semibold text-5xl motion-delay-300 motion-opacity-in-0 motion-preset-expand">I build cool shi.</h1>
+      <p className="motion-delay-500 motion-opacity-in-0 motion-preset-expand">check out my <Link href="https://qr.sssphe.com" className="underline">QR Code Generator</Link> tool</p>
     </div>
   )
 }
